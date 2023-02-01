63911b231ed793f404bd4a94b6f145514ee4b0c8
tonoy <iamtanaylodh@gmail.com>
2023-02-01 16:41:32 +0530

    built input component

--- src/App.vue
+++ src/App.vue
@@ -1,52 +1,23 @@
 <script lang="ts">
-import { mapActions, mapState } from "pinia";
-import { useCounterStore } from "./stores/counter";
+import TodoInput from "./components/TodoInput.vue";
 
 export default {
-  computed: {
-    ...mapState(useCounterStore, ["count", "double"]),
-  },
-  methods: {
-    ...mapActions(useCounterStore, ["increment", "decrement"]),
+  components: {
+    TodoInput,
   },
 };
 </script>
 
 <template>
-  <div class="container">
-    <div class="wrapper">
-      <span>Number: {{ count }}</span>
-      <span>Double: {{ double }}</span>
-      <div>
-        <button class="counterButton" @click="decrement">Decrement</button>
-        <button class="counterButton" @click="increment">Increment</button>
-      </div>
-    </div>
+  <div :class="$style.container">
+    <TodoInput />
   </div>
 </template>
 
-<style scoped>
-@import "./assets/variables.css";
+<style module>
 .container {
   min-height: 100vh;
   display: grid;
   place-items: center;
-}
-.wrapper {
-  text-align: center;
-  display: grid;
-  font-size: 2rem;
-}
-.counterButton {
-  background: var(--cyan);
-  border: 1px var(--dark-cyan-50) solid;
-  padding: 2px 5px;
-  margin: 0 5px;
-  margin-top: 1rem;
-  color: var(--dark-cyan);
-  font-size: 0.5em;
-}
-.counterButton:active {
-  background: var(--dark-cyan-50);
 }
 </style>
---
+++ src/components/TodoInput.vue
@@ -0,0 +1,83 @@
+<script lang="ts">
+export default {
+  data() {
+    let text: string = "";
+    return {
+      text,
+    };
+  },
+  methods: {
+    consoleItem() {
+      const item = {
+        id: "",
+        todoText: this.text,
+        done: false,
+      };
+      console.log(item);
+      this.text = "";
+    },
+  },
+};
+</script>
+
+<template>
+  <div :class="$style.container">
+    <form>
+      <input :class="$style['text-input']" type="text" v-model="text" />
+    </form>
+    <button
+      :class="[
+        $style['add-button'],
+        { [$style['add-button-enabled']]: text.length > 0 },
+      ]"
+      @click="consoleItem"
+    >
+      Add Item
+    </button>
+  </div>
+</template>
+
+<style module>
+.container {
+  width: 100%;
+  max-width: 450px;
+  text-align: center;
+}
+.text-input {
+  width: 100%;
+  height: 2rem;
+  line-height: 2rem;
+  font-size: 1em;
+  background: transparent;
+  border: 0;
+  border-bottom: 0.23rem var(--dark-cyan) solid;
+  color: var(--dark-cyan);
+  padding: 1px 0.5rem;
+  opacity: 0.2;
+  outline: none;
+  transition: opacity 200ms;
+}
+.text-input:focus {
+  opacity: 1;
+}
+.add-button {
+  font-size: 1.2em;
+  margin-top: 15px;
+  padding: 0.2rem 1rem;
+  background: var(--cyan);
+  border: 1px solid var(--dark-cyan);
+  border-radius: 4px;
+  cursor: pointer;
+  float: right;
+  opacity: 0;
+  transition: opacity 150ms;
+}
+
+.add-button:active {
+  background: var(--bright-cyan);
+}
+
+.add-button-enabled {
+  opacity: 1;
+}
+</style>
